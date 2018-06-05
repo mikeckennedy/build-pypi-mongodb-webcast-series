07b24f044ff2f488b69d799aab51dd3299270979
Michael Kennedy <mikeckennedy@gmail.com>
2018-06-05 16:40:49 -0700

    Some starter data and classes that are an exact match to my larger DB that we'll work with during part 2.

---
+++ part2/final_data_classes/users.py
@@ -0,0 +1,17 @@
+import datetime
+import mongoengine
+
+
+class User(mongoengine.Document):
+    email = mongoengine.StringField()
+    name = mongoengine.StringField()
+    created_date = mongoengine.DateTimeField(default=datetime.datetime.now)
+
+    meta = {
+        'db_alias': 'core',
+        'collection': 'users',
+        'indexes': [
+            'created_date',
+            'email',
+        ]
+    }
